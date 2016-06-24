79e6023cf251d9235780394ac9b234fc66eb776c
Jimmy Lin <usmcjimmy@gmail.com>
2016-06-24 13:50:03 -0400

    basic starter with node express

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+// const serviceRoutes = require(path.join(__dirname, 'routes/services'));
+
+/* app setting */
+const port = process.env.PORT || 3000;
+const app = express();
+const server = app.listen(port);
+const request = require('request');
+
+// parse incoming forms
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// static route to public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// log
+app.use(logger('dev'));
+
+/*Views*/
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+/* Routes */
+// app.use('/services', serviceRoutes)
+
+app.get('/', function(req, res){
+  res.render('pages/index')
+})
