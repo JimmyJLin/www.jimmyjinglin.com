90f87b3868570a7b254fae68e14051c39e5a4b3e
Jimmy Lin <usmcjimmy@gmail.com>
2016-07-07 22:07:24 -0400

    added sass-middleware

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
-// const serviceRoutes = require(path.join(__dirname, 'routes/services'));
 
 /* app setting */
 const port = process.env.PORT || 3000;
 const app = express();
 const server = app.listen(port);
 const request = require('request');
+
+// sass setting
+const connect = require('connect');
+const sassMiddleware = require('node-sass-middleware');
+const srcPath = __dirname + '/sass';
+const destPath = path.join(__dirname + '/public/css');
+
+// sassMiddleware
+app.use(sassMiddleware({
+  src: srcPath,
+  dest: destPath,
+  debug: true,
+  outputStyle: 'compressed',
+  prefix: '/prefix'
+}));
 
 // parse incoming forms
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,9 +37,6 @@ app.use(logger('dev'));
 /*Views*/
 app.set('views', './views');
 app.set('view engine', 'ejs');
-
-/* Routes */
-// app.use('/services', serviceRoutes)
 
 app.get('/', function(req, res){
   res.render('pages/index')
