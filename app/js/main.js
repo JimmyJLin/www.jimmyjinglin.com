7fc55b11853386b69ce29074b57e41d3a4053ec5
Jimmy Lin <usmcjimmy@gmail.com>
2016-11-25 10:31:54 -0500

    added jquery

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,14 @@
 $(function(){
   console.log('script file loaded')
 
-
+  // Toggle Buttons - add class active if clicked and remove if click again
+  $('.button.toggle').on("click", function(){
+    if($(this).hasClass('selected')){
+      $(this).removeClass('selected').text('Vote')
+    } else {
+      $(this).addClass('selected').text('Voted')
+    }
+  })
 
 
 
