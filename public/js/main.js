1e822e5273777c5969d33b8f0fcc12cc9b242ea2
Jimmy Lin <usmcjimmy@gmail.com>
2017-01-23 21:21:44 -0500

    added favicon

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
-$(function(){
+$(() => {
   console.log('script file loaded');
 
   // Toggle Buttons - add class active if clicked and remove if click again
-  $('.button.toggle').on("click", function(){
+  $('.button.toggle').on("click", () => {
     if($(this).hasClass('selected')){
       $(this).removeClass('selected').text('Vote')
     } else {
