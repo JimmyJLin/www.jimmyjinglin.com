ba30dda5ed5fc6613c31be4c3e6d6ebf58a2749d
Jimmy Lin <usmcjimmy@gmail.com>
2017-02-09 17:29:16 -0500

    added 4 icons for highlights partial

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,32 @@
 $(() => {
   console.log('script file loaded');
 
-  // Footer image jquery
+  // Highlgihts partial background-image jquery
+  $('#section-one').on('mouseover', () => {
+    $('#one').css('background-image', 'url("/images/social-medias/usmc-filled.png")');
+  });
+  $('#section-one').mouseout(() => {
+    $('#one').css('background-image', 'url("/images/social-medias/usmc.png")');
+  });
+  $('#section-two').on('mouseover', () => {
+    $('#two').css('background-image', 'url("/images/social-medias/education-filled.png")');
+  });
+  $('#section-two').mouseout(() => {
+    $('#two').css('background-image', 'url("/images/social-medias/education.png")');
+  });
+  $('#section-three').on('mouseover', () => {
+    $('#three').css('background-image', 'url("/images/social-medias/work-filled.png")');
+  });
+  $('#section-three').mouseout(() => {
+    $('#three').css('background-image', 'url("/images/social-medias/work.png")');
+  });
+  $('#section-four').on('mouseover', () => {
+    $('#four').css('background-image', 'url("/images/social-medias/entrepreneur-filled.png")');
+  });
+  $('#section-four').mouseout(() => {
+    $('#four').css('background-image', 'url("/images/social-medias/entrepreneur.png")');
+  });
+  // Footer background-image jquery
   $('#github').on('mouseover', () => {
     $('#github').css('background-image', 'url("/images/social-medias/github-red.png")');
   });
